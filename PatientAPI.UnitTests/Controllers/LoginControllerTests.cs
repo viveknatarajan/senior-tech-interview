42f8d5753d9b791a96d94a70a53911e3b41995c0
viveknatarajan <natvivek@gmail.com>
2023-02-24 03:10:55 +0530

    Removed the test project.

---
+++ PatientAPI.UnitTests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using PatientAPI.Controllers;
+using PatientAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PatientAPI.UnitTests.Controllers
+{
+    public class LoginControllerTests
+    {
+        LoginController sut;
+        ILoginService mockLoginService;
+
+        public LoginControllerTests()
+        {
+            mockLoginService = Substitute.For<ILoginService>();
+            sut = new LoginController(mockLoginService);
+        }
+
+        [Fact]
+        public void Login_ShouldReturnBadRequest_ForEmptyCredential()
+        {
+            //var response = sut.Login(null) as BadRequestObjectResult;
+            //response.Should().NotBeNull();
+            //response.StatusCode.Should().Be(400);
+        }
+    }
+}
